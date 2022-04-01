2a07eb2016f6f8fa89fb91f90fc2fb687473747c
juliano.hein <juliano.hein@azi.com.br>
2022-03-31 21:08:27 -0400

    inmemory

--- DesafioSGI/Startup.cs
+++ DesafioSGI/Startup.cs
@@ -26,7 +26,7 @@ namespace DesafioSGI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<Context>(options => options.UseInMemoryDatabase("inmemory"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
